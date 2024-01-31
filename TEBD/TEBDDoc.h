52197884aed7a0bfb24b61c023765d206c73790d
Adrian Roman <aroman_ro@yahoo.com>
2024-01-31 15:20:39 +0200

    Minor

--- TEBD/TEBDDoc.h
+++ TEBD/TEBDDoc.h
@@ -16,7 +16,6 @@ protected: // create from serialization only
 	CTEBDDoc();
 	DECLARE_DYNCREATE(CTEBDDoc)
 	DECLARE_MESSAGE_MAP()
-
 public:
 	Chart m_Chart;
 
@@ -30,22 +29,18 @@ public:
 	afx_msg void OnFileStart();
 	afx_msg void OnUpdateFileStart(CCmdUI* pCmdUI);
 
-
-	BOOL OnNewDocument() override;
-	void Serialize(CArchive& ar) override;
-#ifdef SHARED_HANDLERS
-	void InitializeSearchContent() override;
-	void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds) override;
-#endif // SHARED_HANDLERS
-
 	~CTEBDDoc() override;
 #ifdef _DEBUG
 	void AssertValid() const override;
 	void Dump(CDumpContext& dc) const override;
 #endif
 
-protected:
+private:
+	BOOL OnNewDocument() override;
+	void Serialize(CArchive& ar) override;
 #ifdef SHARED_HANDLERS
+	void InitializeSearchContent() override;
+	void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds) override;
 	// Helper function that sets search content for a Search Handler
 	void SetSearchContent(const CString& value);
 #endif // SHARED_HANDLERS
